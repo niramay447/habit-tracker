c8b6f3319a1e71b8af1031084213a15bc93336d0
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-03-22 11:32:40 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 USERNAME = "niramay"
 TOKEN = "add_token_here"
+GRAPH_ID = "graph123"
 user_params = {
     "token" : TOKEN,
     "username" : USERNAME,
@@ -17,7 +18,7 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 graph_config = {
-    "id": "graph123",
+    "id": GRAPH_ID,
     "name": "Cycling Graph",
     "unit": "Km",
     "type": "float",
@@ -25,5 +26,15 @@ graph_config = {
 }
 response = requests.post(url=graph_endpoint,json=graph_config,headers=headers)
 
-# response = requests.post(url=PIXELA_END_POINT, json=user_params)
+#  response = requests.post(url=PIXELA_END_POINT, json=user_params)
+# print(response.text)
+
+pixel_creation_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
+
+pixel_data = {
+    "date": "20210322",
+    "quantity": "9.74",
+}
+
+response = requests.post(url= pixel_creation_endpoint,json=pixel_data,headers=headers)
 print(response.text)
